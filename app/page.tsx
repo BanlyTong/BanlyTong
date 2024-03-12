1b574cda45a7c7f92e6a8893740ba31f5a1b35ed
BanlyTong <banlytong@gmail.com>
2024-03-12 14:36:28 +0700

    feat: set up lint

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,7 @@ export default function Page() {
       <div className="h-80 flex flex-col items-start justify-end space-y-2">
         <h1 className="text-3xl font-bold">Hey, There!</h1>
         <p>
-          I'm Banly, this is my personal space where I'm sharing my journey as a
-          software professional with more than 6 years of experiences.
+          I&#39;m Banly, a software engineer, this is my personal space where I&#39;m sharing my professional journey with more than 6 years of experiences.
         </p>
       </div>
     </div>
